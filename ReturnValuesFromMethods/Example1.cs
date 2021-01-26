675783183a1528290b0c173d49a01f248f6cf4d5
Panu Valtanen <p4nu@users.noreply.github.com>
2021-01-26 20:32:26 +0200

    Add Hello World example.

---
+++ ReturnValuesFromMethods/Example1.cs
@@ -0,0 +1,17 @@
+using System;
+
+namespace ReturnValuesFromMethods
+{
+    public class Example1
+    {
+        public Example1()
+        {
+            Start();
+        }
+
+        private void Start()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
--- ReturnValuesFromMethods/Program.cs
+++ ReturnValuesFromMethods/Program.cs
@@ -4,6 +4,7 @@
     {
         public static void Main(string[] args)
         {
+            var example1 = new Example1();
         }
     }
 }
